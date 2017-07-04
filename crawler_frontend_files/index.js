cd7c6e0adf99c479015a4cb32d78206137e2a570
Sridevi Akondi <sridevi.a@osmosys.in>
2017-07-05 01:32:14 +0530

    Updated UI- jQuery and Bootstrap

--- crawler_frontend_files/index.js
+++ crawler_frontend_files/index.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function ($) {
     console.log("test");
+    $body = $("body");
+    //$("button").on('click','(function (e) {
     $("button").click(function (e) {
+         $("button").attr('disabled',true);
+         $("button").css('color','#9E9E9E');
+         $("#img1").css('display','block');
         console.log($('#txtURL').val());
         console.log($('#txtKeyword').val());
         $.get("http://localhost:8181/CrawlerService/rest/search/query?url=" + $('#txtURL').val() + "&keyword=" + $('#txtKeyword').val()).done(function (data) {
@@ -18,8 +23,12 @@ jQuery(document).ready(function ($) {
     function testcall(somedata) {
         var newpage = "_blank";
         $.ajax({
-            'url': "http://localhost:8983/solr/select/?q=" + $('#txtKeyword').val() + "&wt=json&json.wrf=?&indent=true",
+            'url': "http://localhost:8983/solr/select/?q=" + $('#txtKeyword').val() + "&wt=json&json.wrf=?&indent=true&start=0&rows=300",
             'success': function (result) {
+                $("button").attr('disabled',false);
+                $("button").css('color','#000');
+                $("#img1").css('display','none');
+                $("#res").css('display','block');
                 var seconds = result.responseHeader.QTime / 1000;
                 var Parent = document.getElementById('rs');
                 var heading = "<div id='search'><h2> Search Results</h2><p>" + result.response.numFound + " Results in " + seconds + " seconds </p></div>"
@@ -30,15 +39,7 @@ jQuery(document).ready(function ($) {
                     var text1 = $('#txtKeyword').val();
                     var myRegex = new RegExp(text1, "ig");
                     var content1 = result.response.docs[i].content.replace(myRegex, "<span class='highlight'>" + $('#txtKeyword').val() + "</span>");
-                    var input = content1;
-                    Algorithmia.client("sim1loEEquzQCtITLK9zWo4Rpsz1")
-                        .algo("algo://nlp/Summarizer/0.1.6")
-                        .pipe(input)
-                        .then(function (output) {
-                            console.log(JSON.stringify(output));
-                        });
-
-                    var thisResult = "<div><a href=" + result.response.docs[i].url + " target=" + newpage + ">" + result.response.docs[i].title + "</a> <p id='link'>" + result.response.docs[i].url + "</p> <div class='innerText'>" + JSON.stringify(output) + "</div> <br></div>";
+                    var thisResult = "<div id='list'><a href=" + result.response.docs[i].url + " target=" + newpage + ">" + result.response.docs[i].title + "</a> <p id='link'>" + result.response.docs[i].url + "</p> <div class='innerText'>" + content1 + "</div> <br></div>";
                     var NewDiv = document.createElement("DIV");
                     NewDiv.innerHTML = thisResult;
                     Parent.appendChild(NewDiv);
@@ -49,29 +50,6 @@ jQuery(document).ready(function ($) {
         });
     }
 
-
-
-
-
-
-
-    /* function testcall(somedata) {
-          $.ajax({
-    'url': "http://localhost:8983/solr/select/?q="+ $('#txtKeyword').val() + "&wt=json&json.wrf=?&indent=true",
-    'success': function(result) {  var Parent = document.getElementById('rs');
-              for (var i = 0; i < result.response.docs.length; i++) {
-                  var thisResult = "<b>" + result.response.docs[i].boost + "</b><br>" + result.response.docs[i].title
-                      + "<br>" + result.response.docs[i].content + "<br>";
-                  var NewDiv = document.createElement("DIV");
-                  NewDiv.innerHTML = thisResult;
-                  Parent.appendChild(NewDiv);
-              }},
-    'dataType': 'jsonp',
-    'jsonp': 'json.wrf'
-      });   
-    }  */
-
-    // $(txtURL).val('');
-    //  $(txtKeyword).val('');
+    
 });
 
